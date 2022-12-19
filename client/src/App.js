dfffd03e7d016de67feffa61911b0875f6a34432
DarkkLegion <arshad18syed@gmail.com>
2022-12-19 20:48:46 +0530

    implemented SignUp page base

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Login from'./components/login';
+import SignUp from './components/signUp';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   console.log("Hello world");
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signUp" element={<SignUp  />} /> 
+        </Routes> 
+      </Router>
     </div>
   );
 }
