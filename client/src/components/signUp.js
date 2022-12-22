da5fd0bbba46a71e37dd142a72d97b031aee0db4
DarkkLegion <arshad18syed@gmail.com>
2022-12-22 17:59:49 +0530

    Signup Validation with Regex

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -7,6 +7,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
+import FormHelperText from '@mui/material/FormHelperText';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -17,11 +18,12 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {useNavigate} from "react-router-dom";
 import '@fontsource/roboto/400.css'
 import BgImage from '../assets/signUpBg.jpg'; 
+import Axios from 'axios';
+
 
 const theme = createTheme();
 const styles = {
   heroContainer: {
-    height: 800,
     backgroundImage: `url(${BgImage})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
@@ -29,8 +31,37 @@ const styles = {
     margin: -24,
     padding: 24,
   }
- };
+};
+let flag = false; // Flag used to check if everything is validated
 
+function validateEmail(email){
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (email.match(validRegex)) { return false;} 
+  else {flag=true; return true;}
+ }
+function validateName(name){
+  if(name === '' || name.length<3){return true;}
+}
+function validateVoterID(voterid){
+  let regex = new RegExp(/^[A-Z]{3}[0-9]{7}$/);
+  if (regex.test(voterid)) { return false;}
+  else return true;
+}
+function validatePhone(phone){
+  let regex = new RegExp(/^[7-9][0-9]{9}$/)
+  if(regex.test(phone)) {return false;}
+  else  return true;
+}
+function validatePassword(passwd){
+  let regex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
+  if(regex.test(passwd)){return false;}
+  else return true;
+}
+function validateDOB(Dob){
+  let regex = new RegExp(/(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).([19,20]{2})+([0-9]{2})/);
+  if(regex.test(Dob)){return false;}
+  else return true;
+}
 
 export default function SignUp() {
   let history = useNavigate()
@@ -45,9 +76,43 @@ export default function SignUp() {
   const [emailError, setEmailError] = React.useState(false);
   const [passwordError, setPasswordError] = React.useState(false);
   const [DOBError, setDOBError] = React.useState(false);
-  const [nationalityError, setNationalityError] = React.useState('');
+  const [nationalityError, setNationalityError] = React.useState(false);
   const [nationality, setNationality] = React.useState('');
   const [PhoneNumberError, setPhoneNumberError] = React.useState(false)
+
+
+  const errorCheck = (data) =>{
+    if(validateName(data.get('Name'))){setNameError(true);}
+    if(validateVoterID(data.get('voterID'))){setvoterIDError(true);}
+    if(validatePhone(data.get('PhoneNumber'))){setPhoneNumberError(true);}
+    if(validateEmail(data.get('email'))){setEmailError(true);}
+    if(validatePassword(data.get('password'))){setPasswordError(true);}
+    if(validateDOB(data.get('DateOfBirth'))){setDOBError(true);}
+    if(nationality === ''){setNationalityError(true);}
+    if(
+      !validateName(data.get('Name')) &&
+      !validateVoterID(data.get('voterID')) &&
+      !validatePhone(data.get('PhoneNumber')) && 
+      !validateEmail(data.get('email')) &&
+      !validatePassword(data.get('password')) &&
+      !validateDOB(data.get('DateOfBirth'))
+    ){
+      console.log("Congrats! Everything is validated!")
+      flag=true;
+    }
+    else { 
+      console.log("Not Validated! Please check below to see what went wrong!");
+      flag=false;
+      console.log({
+      name: validateName(data.get('Name')),
+      voterID: validateVoterID(data.get('voterID')),
+      phone: validatePhone(data.get('PhoneNumber')),
+      email: validateEmail(data.get('email')),
+      password: validatePassword(data.get('password')),
+      DOB: validateDOB(data.get('DateOfBirth')),
+    });
+  }
+  }
 
   const handleChange = (event) => {
     setNationality(event.target.value);
@@ -57,21 +122,10 @@ export default function SignUp() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    setNameError(false);
-    setvoterIDError(false);
-    setEmailError(false);
-    setPasswordError(false);
-    setDOBError(false);
-    setPhoneNumberError(false);
-    setNationalityError(false);
-    if(data.get('Name') === ''){setNameError(true);}
-    if(data.get('voterID') === ''){setvoterIDError(true);}
-    if(data.get('email') === ''){setEmailError(true);}
-    if(data.get('password') === ''){setPasswordError(true);}
-    if(data.get('DateOfBirth') === ''){setDOBError(true);}
-    if(nationality === ''){setNationalityError(true);}
-    if(data.get('PhoneNumber') === ''){setPhoneNumberError(true);}
+    errorCheck(data);
     //TODO add error checking here and then push to node js
+    
+    // Axios.post()
 
     console.log({
       name: data.get('Name'),
@@ -95,7 +149,6 @@ export default function SignUp() {
       
         <Box
           sx={{
-            marginBottom: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
@@ -161,7 +214,7 @@ export default function SignUp() {
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
+              <TextField
                   required
                   fullWidth
                   name="password"
@@ -171,7 +224,7 @@ export default function SignUp() {
                   autoComplete="new-password"
                   error={passwordError}
                   onChange={() => setPasswordError(false)}
-                  helperText={passwordError ? "Please enter a password between 6-16 characters" : ''}
+                  helperText={passwordError ? "Please enter a password between 6-16 characters with at least one uppercase letter, one lowercase letter and one number." : ''}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -195,12 +248,14 @@ export default function SignUp() {
                     value={nationality}
                     label="Nationality"
                     error={nationalityError}
-                    helperText={nationalityError ? "Please select your nationality" : ''}
                     onChange={handleChange}
                   >
                     <MenuItem value="Indian">Indian</MenuItem>
                     <MenuItem value="Foreign">Foreign</MenuItem>
                   </Select>
+                  <FormHelperText
+                    error={nationalityError}
+                  >{nationalityError ? "Please select your nationality" : ''}</FormHelperText>
                 </FormControl>
               </Grid>
             </Grid>
