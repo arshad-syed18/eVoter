f944e838e06514a60a4685ad6563a334ed5cb752
DarkkLegion <arshad18syed@gmail.com>
2023-01-10 20:20:34 +0530

    bug fixes

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ErrorPage() {
     return(
         <div>
-            ERROR! 404 NOT FOUND
+            ERROR! 404 NOT FOUND! Please Go back to login page and try again!
         </div>
     );
 }
---
+++ client/src/components/homeDashboard.js
@@ -0,0 +1,7 @@
+import * as React from React;
+
+export default function homeDashboard() {
+    return(
+        <div>Design a dashboard with charts etc</div>
+    )
+}
